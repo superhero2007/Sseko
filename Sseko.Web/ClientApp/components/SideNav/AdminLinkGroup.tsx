f4ad515091d19d0faf0de3ce0703f5ae341e770c
Miroslaw Opoka <miroslaw@creativegurus.com>
2017-10-08 00:35:44 +0200

    Admin Icon

--- Sseko.Web/ClientApp/components/SideNav/AdminLinkGroup.tsx
+++ Sseko.Web/ClientApp/components/SideNav/AdminLinkGroup.tsx
@@ -1,5 +1,10 @@
 ﻿import * as React from 'react';
 import { NavBarLinkSingle } from './NavBarLinkSingle';
+const dashboardIcon = require<string>('../../img/dashboard-ico.png');
+const linkIcon = require<string>('../../img/link-ico.png');
+
+const dashboardIconActive = require<string>('../../img/dashboard-active-ico.png');
+const linkIconActive = require<string>('../../img/link-active-ico.png');
 
 
 export const AdminLinkGroup = () => {
@@ -8,10 +13,14 @@ export const AdminLinkGroup = () => {
             <NavBarLinkSingle
                 href='/Dashboard'
                 label='Dashboard'
+                imgSrc={dashboardIcon}
+                imgActiveSrc={dashboardIconActive}
             />
             <NavBarLinkSingle
                 href='/Manage/Users'
                 label='Manage Users'
+                imgSrc={linkIcon}
+                imgActiveSrc={linkIconActive}
             />
         </ul>
     );
